1509103ea5d6be6422abe46a04b05a3faad87be4
emrgem <emrgem@bergen.org>
2024-09-19 08:49:33 -0400

    ternary practice

---
+++ Unit 0 - Python Basics/Lesson4-conditionals/ternary.py
@@ -0,0 +1,17 @@
+# true_val if condition else false_val
+# x = 10
+# if x > 5:
+#     result = "Greater"
+# else:
+#     result = "Smaller"
+# result = "Greater" if x > 5 else "Smaller"
+
+# Movie Ticket Pricing
+#Determine the price of a movie based on age and time of the day
+
+age = 17
+time = 14
+price = 5 if age < 12 else 7 if age <= 18 else 10
+price -= 2 if time <16 else 0 #Discount before 4pm
+result = f"Ticket Price: ${price}"
+print(result) 
