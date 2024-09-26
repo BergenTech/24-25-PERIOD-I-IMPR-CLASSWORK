390835023e8d91c25f4ec27b9724905b0e544062
emrgem <emrgem@bergen.org>
2024-09-26 08:59:46 -0400

    9/26/24 - nested loops

---
+++ Unit 0 - Python Basics/Lesson5-Loops/3_nested_loops/exercise.py
@@ -0,0 +1,16 @@
+# for i in range(3):
+#     for j in range(3):
+#         # print('Hello BT!')
+#         # print(i*j)
+#         print(f"i:{i}-j:{j}")
+
+# for i in range(3):
+#     j = 0
+#     while j<3:
+#         print(f"i:{i}-j:{j}")
+#         j+=1
+
+for i in range(4):
+    print(f'Outer for loop: {i}')
+    for j in range(4):
+        print(f'    Inner for loop: {j}')
