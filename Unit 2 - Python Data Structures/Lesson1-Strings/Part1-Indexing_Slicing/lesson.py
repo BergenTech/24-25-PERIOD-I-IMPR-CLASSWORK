fb2bbbf95a93b02f9091ab9abb7e96d38535c159
emrgem <emrgem@bergen.org>
2024-11-13 09:32:46 -0500

    indexing

---
+++ Unit 2 - Python Data Structures/Lesson1-Strings/Part1-Indexing_Slicing/lesson.py
@@ -0,0 +1,11 @@
+# Positive indexing - zero-based
+fruit = 'apple'
+print(fruit[0]) #first character
+print(fruit[4]) #last character
+print(fruit[len(fruit)-1]) #last character
+
+#Negative indexing - -1 last element
+print(fruit[-1]) #last element
+print(fruit[-5]) #first element
+print(fruit[-len(fruit)]) #first element
+
