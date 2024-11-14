3328ae1060c90489536c230a693bf33de9625665
emrgem <emrgem@bergen.org>
2024-11-14 09:42:09 -0500

    Format Email

--- Unit 2 - Python Data Structures/Lesson1-Strings/Part3_String_Methods/exercise.py
+++ Unit 2 - Python Data Structures/Lesson1-Strings/Part3_String_Methods/exercise.py
@@ -1,3 +1,26 @@
+# def format_email(email):
+#     # Step 1: Remove leading and trailing spaces
+#     email = email.strip()
+#     # Step 2: Remove spaces 
+#     email = email.replace(" ", "")
+#     # Step 3: Convert to lowercase
+#     return email.lower()
+#     # Method Chaining
+#     return email.strip().replace(" ", "").lower()
+
+
+
+
+# # Test cases
+# email1 = " User@Example.com "
+# email2 = "user @ example.com"
+# email3 = "USER@EXAMPLE.COM"
+
+
+# print(format_email(email1)) #output: user@example.com
+# print(format_email(email2)) #output: user@example.com
+# print(format_email(email3)) #output: user@example.com
+
 def find_substring(text:str, sub:str):
     count = text.count(sub)
     first_occurence = text.find(sub)
