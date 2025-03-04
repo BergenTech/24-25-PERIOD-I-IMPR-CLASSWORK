6dc2e75ee112128fbca78ccb493740c96576b927
emrgem <emrgem@bergen.org>
2025-03-04 08:25:25 -0500

    Unit 4 Lesson 1

---
+++ Unit 4 - File Handling/Lesson1_Reading Files/lesson.py
@@ -0,0 +1,40 @@
+#1 Function to read entire file content
+def read_entire_file(filename):
+    """Reads the entire content of a file and returns it as a string."""
+    pass
+
+
+#2 Function to read file line by line
+def read_file_line_by_line(filename):
+    """Reads a file line by line and prints each line without extra whitespace."""
+    pass
+
+
+#3 Function to store lines as a list
+def read_file_as_list(filename):
+    """Reads all lines of a file and returns them as a list."""
+    pass
+
+
+#3-2 Function to store lines as a list readlines() includes \n
+def read_file_as_list(filename):
+    """Reads all lines of a file and returns them as a list."""
+    pass
+
+
+#4 Function to count lines in a file
+def count_lines(filename):
+    """Counts the number of lines in a file."""
+    pass
+
+
+#5 Function to search for a keyword in a file
+def search_keyword(filename, keyword):
+    """Finds and prints lines that contain a specific keyword."""
+    pass
+
+
+
+# Usage
+if __name__ == "__main__":
+	pass
