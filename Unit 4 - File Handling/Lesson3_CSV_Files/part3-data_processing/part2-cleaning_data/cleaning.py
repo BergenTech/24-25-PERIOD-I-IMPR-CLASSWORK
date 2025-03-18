cd4b73e03d3f0812da3dbc0211d2cf6b06fb1e02
emrgem <emrgem@bergen.org>
2025-03-18 08:58:54 -0400

    clean movie data

--- Unit 4 - File Handling/Lesson3_CSV_Files/part3-data_processing/part2-cleaning_data/cleaning.py
+++ Unit 4 - File Handling/Lesson3_CSV_Files/part3-data_processing/part2-cleaning_data/cleaning.py
@@ -11,8 +11,29 @@ def print_movie_info(movies, num_movies=5):
     print(f"Showing {min(num_movies, len(movies))} movies:")
     for i in range(min(num_movies, len(movies))):
         movie = movies[i]
-        print(movie)
-        # print(f"ID: {movie['Movie_ID']}, Title: {movie['Title']}, Year: {movie['Release_Year']}, Rating: {movie['Rating']}" )
+        # print(movie)
+        print(f"ID: {movie['Movie_ID']}, Title: {movie['Title']}, Year: {movie['Release_Year']}, Rating: {movie['Rating']}" )
+
+def clean_movie_data(movies):
+    cleaned_movies = []
+    seen_ids = set()
+    for movie in movies:
+        #skip if we've seen this movie id before
+        if movie["Movie_ID"] in seen_ids:
+            continue
+        # Add to set of seen IDs
+        seen_ids.add(movie["Movie_ID"])
+        # Make a copy of the movie to clean
+        clean_movie = movie.copy()
+        #1 - Fix the capitalization in titles
+        clean_movie["Title"] = clean_movie['Title'].strip().title()
+        #2 - Handle missing values
+        if not clean_movie['Rating'] or clean_movie['Rating'].strip()=="":
+            clean_movie['Rating'] = "0.0"
+        if not clean_movie['Genre'] or clean_movie['Genre'].strip()=="":
+            clean_movie['Genre'] = "Unknown"
+        cleaned_movies.append(clean_movie)    
+    return cleaned_movies
 
 def main():
     print("=== Part 1: Reading and Exploring Data ===")
@@ -20,7 +41,11 @@ def main():
     movies = read_csv_file("movie_data.csv")
     print(f"Total movies in dataset: {len(movies)}")
     # Step 2: Print the total number of movies
-    print_movie_info(movies,200)
-
+    print_movie_info(movies) # will print first 5 as default
+    # print_movie_info(movies, 1) # will print only 1 movie
+    # print_movie_info(movies, 200) # will for the length of the list of dict
+    # Step 3 - Display the first 3 cleaned movies
+    cleaned_movies = clean_movie_data(movies)
+    print_movie_info(cleaned_movies,3)
 if __name__ == "__main__":
     main()
