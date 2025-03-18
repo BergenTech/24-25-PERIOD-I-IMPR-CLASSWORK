cc0f1e46930f050ee534cb9803ec0281d2097f04
emrgem <emrgem@bergen.org>
2025-03-18 08:22:07 -0400

    part 2 cleaning dat a starter

---
+++ Unit 4 - File Handling/Lesson3_CSV_Files/part3-data_processing/part2-cleaning_data/cleaning.py
@@ -0,0 +1,30 @@
+import csv
+def read_csv_file(file_path):
+    movies = []
+    with open(file_path, 'r', encoding="utf-8", newline='') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            movies.append(row)
+    return movies
+
+def print_movie_info(movies, num_movies=5):
+    print(f"Showing {min(num_movies, len(movies))} movies:")
+    for i in range(min(num_movies, len(movies))):
+        movie = movies[i]
+        print(movie)
+        # print(f"ID: {movie['Movie_ID']}, Title: {movie['Title']}, Year: {movie['Release_Year']}, Rating: {movie['Rating']}" )
+
+def main():
+    print("=== Part 1: Reading and Exploring Data ===")
+    
+    # Step 1: Read the movie_data.csv file
+    # Your code will go here
+    movies = read_csv_file("movie_data.csv")
+    print(f"Total movies in dataset: {len(movies)}")
+    
+    # Step 2: Print the total number of movies
+    # Your code will go here
+    print_movie_info(movies,200)
+
+if __name__ == "__main__":
+    main()
