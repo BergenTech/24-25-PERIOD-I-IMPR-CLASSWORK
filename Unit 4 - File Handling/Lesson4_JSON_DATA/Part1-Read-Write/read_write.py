8dcb9727453a704df726017fff295e6a21167db2
emrgem <emrgem@bergen.org>
2025-03-19 10:03:49 -0400

    json read write completed

--- Unit 4 - File Handling/Lesson4_JSON_DATA/Part1-Read-Write/read_write.py
+++ Unit 4 - File Handling/Lesson4_JSON_DATA/Part1-Read-Write/read_write.py
@@ -9,10 +9,14 @@ print("\nExercise 1: Parse a JSON string")
 state_json = '{"name": "California", "abbreviation": "CA", "area_codes": ["209", "213", "310"]}'
 
 # TODO: Use json.loads() to parse the JSON string into a Python dictionary
+state_dict = json.loads(state_json)
+# print(type(state_dict))
 # TODO: Print the state name
+print(f"State name: {state_dict['name']}")
 # TODO: Print the abbreviation
-# TODO: Print the first area code in the list
-
+print(f"Abbreviation: {state_dict['abbreviation']}")
+# TODO: Print the first area code in the list # 209
+print(f"First area code: {state_dict['area_codes'][0]}")
 
 # ------------------------------------------------------
 # Exercise 2: Convert Python to JSON (json.dumps)
@@ -29,21 +33,33 @@ state_dict = {
 }
 
 # TODO: Use json.dumps() to convert the dictionary to a JSON string
+json_string = json.dumps(state_dict)
 # TODO: Print the JSON string
-
+print(f"JSON String: {json_string}")
+print(type(json_string))
 # TODO: Use json.dumps() again, but this time with indentation of 4 spaces
+pretty_json = json.dumps(state_dict, indent=4)
 # TODO: Print the formatted JSON string
-
-
+print("Formatted JSON String")
+print(pretty_json)
 # ------------------------------------------------------
 # Exercise 3: Load JSON from a file (json.load)
 # ------------------------------------------------------
 print("\nExercise 3: Load JSON from a file")
 
 # TODO: Use json.load() to read data from 'states.json'
+with open("states.json", 'r') as file:
+    states_data = json.load(file)
+    # Access data from the file
+    states_list = states_data["states"]
 # TODO: Print how many states are in the file
+    print(f"Number of states: {len(states_list)}")
 # TODO: Print the name of the first state
+    print(f"First state: {states_list[0]['name']}")
+    print(f"Last state: {states_list[-1]['name']}")
+
 # TODO: Print the abbreviation of the last state in the list
+    print(f"Last state Abbreviation: {states_list[-1]['abbreviation']}")
 
 
 # ------------------------------------------------------
@@ -64,9 +80,10 @@ florida = {
 }
 
 # TODO: Use json.dump() to write the florida dictionary to a file named 'florida.json'
+with open('florida.json', 'w') as file:
 # TODO: Make sure to use indentation of 4 spaces to make the file readable
-
-
+    json.dump(florida, file, indent=4)
+    print("Successfully wrote Florida data to florida.json!")
 # ------------------------------------------------------
 # Exercise 5: Working with nested JSON
 # ------------------------------------------------------
